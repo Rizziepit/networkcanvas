8d2dc65541b0c17e8ddf9a31fb5738e2476ebd7a
Rizmari Versfeld <rizziepit@gmail.com>
2014-12-05 19:54:18 +0200

    turn entity and relation rendering code into re-usable functions

--- app.js
+++ app.js
@@ -1,30 +1,73 @@
 (function(){
     var app = angular.module("networkcanvas", []);
 
+    var color = d3.scale.category20();
+
     Relation = function(source, target) {
         this.source = source;
         this.target = target;
+
+        // rendering attributes
     };
 
     Entity = function(name, type) {
         this.name = name;
         this.type = type;
+
+        // rendering attributes
+        this.fixed = true;
+        this.width = 40;
+        this.height = 40;
+
+        this.getLabel = function() {
+            return this.name;
+        };
     };
+
+    renderEntities = function(entities, svg, _cola) {
+        var entity = svg.selectAll(".entity");
+        var label = svg.selectAll(".label");
+        entity
+            .data(entities)
+            .enter()
+            .append("rect")
+            .attr("class", function (d) {return "entity entity-" + d.type;})
+            .attr("width", function (d) {return d.width;})
+            .attr("height", function (d) {return d.height;})
+            .attr("rx", 5).attr("ry", 5)
+            .style("fill", function (d) {return color(1);})
+            .call(_cola.drag);
+        label
+            .data(entities)
+            .enter()
+            .append("text")
+            .attr("class", "label")
+            .text(function (d) {return d.getLabel();})
+            .call(_cola.drag);
+        _cola.start();
+    };
+
+    renderRelations = function(relations, svg, _cola) {
+        var relation = svg.selectAll(".relation");
+        relation
+            .data(relations)
+            .enter()
+            .append("line")
+            .attr("class", "relation");
+        _cola.start();
+    };
+
 
     app.controller("NetworkController", function() {
 
         this.entities = [];
         this.relations = [];
 
-
         /* Canvas setup */
 
         var width = 950;
         var height = 500;
-        var nodeWidth = 40;
-        var nodeHeight = 40;
-        var color = d3.scale.category20();
-        this.entities.push(new Entity("unnamed", "unknown"));
+
         var _cola = cola.d3adaptor()
             .linkDistance(120)
             .avoidOverlaps(true)
@@ -32,8 +75,7 @@
 
         _cola
             .nodes(this.entities)
-            .links(this.relations)
-            .start();
+            .links(this.relations);
 
         var svg = d3.select("#canvas").append('svg')
             .attr("width", width)
@@ -49,8 +91,8 @@
                 .attr("x2", function (d) {return d.target.x;})
                 .attr("y2", function (d) {return d.target.y;});
 
-            entity.attr("x", function (d) {return d.x - nodeWidth / 2;})
-                .attr("y", function (d) {return d.y - nodeHeight / 2;});
+            entity.attr("x", function (d) {return d.x - d.width / 2;})
+                .attr("y", function (d) {return d.y - d.height / 2;});
 
             label.attr("x", function (d) {return d.x;})
                  .attr("y", function (d) {
@@ -64,43 +106,34 @@
 
         this.addEntity = function() {
             this.entities.push(new Entity("unnamed", "unknown"));
-
-            if (svg !== undefined) {
-                var entity = svg.selectAll(".entity");
-                var label = svg.selectAll(".label");
-                entity
-                    .data(this.entities)
-                    .enter()
-                    .append("rect")
-                    .attr("class", function (d) {return "entity entity-" + d.type;})
-                    .attr("width", nodeWidth).attr("height", nodeHeight)
-                    .attr("rx", 5).attr("ry", 5)
-                    .style("fill", function (d) {return color(1);})
-                    .call(_cola.drag);
-                label
-                    .data(this.entities)
-                    .enter()
-                    .append("text")
-                    .attr("class", "label")
-                    .text(function (d) {return d.name;})
-                    .call(_cola.drag);
-                _cola.start();
-            }
+            if (svg !== undefined)
+                renderEntities(this.entities, svg, _cola);
         };
-
 
         this.addRelation = function(source, target) {
             this.relations.push(new Relation(source, target));
+            if (svg !== undefined)
+                renderRelations(this.relations, svg, _cola);
+        };
 
-            if (svg !== undefined) {
-                var relation = svg.selectAll(".relation");
-                relation
-                    .data(this.relations)
-                    .enter()
-                    .append("line")
-                    .attr("class", "relation");
-                _cola.start();
-            }
+        this.lockEntities = function() {
+            this.entities.forEach(function(obj) {obj.fixed = true;});
+        };
+
+        this.unlockEntities = function() {
+            this.entities.forEach(function(obj) {obj.fixed = false;});
+        };
+
+        this.doFlowLayout = function() {
+            this.unlockEntities();
+            _cola.flowLayout("y"); // top-to-bottom
+            this.lockEntities();
+        };
+
+        this.loadData = function() {
+            d3.json("data/rigs.json", function(error, json) {
+
+            });
         };
 
     });
